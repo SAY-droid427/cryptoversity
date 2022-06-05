d5eb89c4ba4e01696e1655b9e7dd7f57bc27c033
SAY-droid427 <sayanimallick0218@gmail.com>
2022-06-05 10:29:05 +0530

    Updated react-scripts

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import '../index.css';
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutLineClose } from 'react-icons/ai';
 import logo from '../images/logo.jpg';
@@ -12,14 +14,14 @@ const NavbarItem=({title, classProps})=>{
 const Navbar = () => {
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
-            <div className="text-white md:flex-[0.5] flex-initial justify-center items-center" >
-                Cryptoversity
-                
+            <div className="md:flex-[0.5] flex-initial justify-center items-center" >
+                <NavbarItem />                
             </div> 
-            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-intial">
+            <ul style={{color:"white", listStyle:"none"}}>
                 {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index)=>(
                     <NavbarItem key={item+index} title={item} />
                 ))}
+                <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]" style={{backgroundColor:"blue", color:"white", padding:"2 7"}}>Login</li>
             </ul>
         </nav>
     )
