79b40013737810333e180a9afd4d37991aea682b
SAY-droid427 <sayanimallick0218@gmail.com>
2022-06-10 23:46:17 +0530

    Added crypto API for stats

---
+++ client/src/services/cryptoDataAPI.js
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const baseURL = process.env.REACT_APP_CRYPTO_API_URL;
+
+const cryptoHeaders = {
+    'x-rapidapi-host': process.env.REACT_APP_RAPIDAPI_HOST,
+    'x-rapidapi-key':process.env.REACT_APP_RAPIDAPI_KEY
+};
+
+const createRequest = (url) => ({
+    url,
+    headers :cryptoHeaders
+});
+
+const cryptoAPI = createApi({
+    reducerPath:'cryptoAPI',
+    baseQuery: fetchBaseQuery({baseURL}),
+    endpoints: (builder) => ({
+        getCoins: builder.query({
+            query: (count) => createRequest(`/coin?limit=${count}`),
+        }),
+
+        getCoinDetails : builder.query({
+            query:(coinId) => createRequest(`/coin/${coinId}`),
+        }),
+
+        getCoinHistory: builder.query({
+          query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+        }),
+      
+        getExchanges: builder.query({
+          query: () => createRequest('/exchanges'),
+
+
+        })
+    })
+});
+
+export const {useGetCoinsQuery,useGetCoinsDetailsQuery,useGetCoinHistoryQuery,useGetExchangesQuery } = cryptoAPI;
+  
